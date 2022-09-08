fa2c549b33ce97b93fcb684eb09fa54b59388b21
AmirbekMirzozoda <amirbekmirzozoda@gmail.com>
2022-09-08 09:50:54 +0500

    Created a library for calculating the area of a circle by radius, a triangle by three sides and an unknown polygon.

---
+++ Square/Base/Figure.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator.Square.Base;
+
+public abstract class Figure
+{
+    private readonly Lazy<double> _square;
+
+    public double Square => _square.Value;
+
+    protected Figure()
+    {
+        _square = new Lazy<double>(CalculateSquare);
+    }
+
+    protected abstract void Validate();
+
+    protected abstract double CalculateSquare();
+}
---
+++ Square/Figures/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using Calculator.Square.Base;
+
+namespace Calculator.Square.Figures;
+
+public class Triangle : Figure
+{
+    public List<Side> Sides { get; }
+    private Lazy<bool> _isRightAngle;
+    public bool IsRightAngle => _isRightAngle.Value;
+
+    public Triangle(params Side[] sides)
+    {
+        Sides = sides.OrderBy(x => x.Length).ToList();
+        _isRightAngle = new Lazy<bool>(CalculateRightAngle);
+        Validate();
+    }
+
+    private bool CalculateRightAngle()
+    {
+        return Math.Abs(Math.Pow(Sides.ElementAt(2).Length, 2) - (Math.Pow(Sides.ElementAt(0).Length, 2) + Math.Pow(Sides.ElementAt(1).Length, 2))) < Vector2D.Tolerance;
+    }
+
+    protected sealed override void Validate()
+    {
+        if (Sides.Count > 3)
+        {
+            throw new ArgumentException("Triangle cannot be with more than 3 sides");
+        }
+
+        if (!(Sides[0].Length < Sides[1].Length + Sides[2].Length || Sides[1].Length < Sides[0].Length + Sides[2].Length || Sides[2].Length < Sides[0].Length + Sides[1].Length))
+        {
+            throw new ArgumentException("Triangle does not exist with this sides");
+        }
+    }
+
+    protected override double CalculateSquare()
+    {
+        var p = 0.5 * Sides.Sum(x => x.Length);
+        return Math.Sqrt(p * (p - Sides.ElementAt(0).Length) * (p - Sides.ElementAt(1).Length) *
+                         (p - Sides.ElementAt(2).Length));
+    }
+}
